85e0478f5e4a369b5ac5ae651fd50a926882ab30
GiorgosG <giorgos.gioumertakis@bel.gr>
2023-10-23 17:00:11 +0300

    first commit

---
+++ MyApplication/MyApplication/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet(Name = "Login")]
+        public string Get()
+        {
+            return "Ok";
+        }
+    }
+}
